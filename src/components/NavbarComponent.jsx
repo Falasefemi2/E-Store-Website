1cd14ca97f225c981f7346a6d32128a64d0679bf
Falase Femi <femifalase228@gmail.com>
2023-03-05 10:42:07 +0100

    use react-router-dom outlet to make the navbar appear in all page

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,7 +1,9 @@
 import { Button, Container, Navbar, Modal } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 
 const NavbarComponent = () => {
   return (
+    <>
     <Navbar expand="sm">
       <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
       <Navbar.Toggle />
@@ -9,6 +11,8 @@ const NavbarComponent = () => {
         <Button>Cart 0 items</Button>
       </Navbar.Collapse>
     </Navbar>
+    <Outlet />
+    </>
   )
 }
 
